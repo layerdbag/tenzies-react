3e8d045c146e0ad6af08e3bd7f54c21c41896a5d
layerdbag <richardoo@live.com>
2023-05-18 16:48:01 +0200

    Added random dice generation event handler

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,59 @@
-// import { useState } from 'react'
+import { useState } from 'react'
+import { nanoid } from 'nanoid'
 
 
 
 // eslint-disable-next-line react/prop-types
-const Die = ({ random }) => {
-  // const selected = {
-  //   backgroundColor: '#59E391',
-  //   borderRadius: '0.4rem',
-  //   border: '2px solid rebeccapurple',
-  // } 
+const Die = ({ value, isHeld, holdDice }) => {
+  
+  const styled = {
+    backgroundColor: isHeld ? '#59E391' : '',
+    borderRadius: isHeld ? 0.4 + 'rem' : '',
+  }
 
   return (
-    <img src={`/images/dice-${random}.png`} />
+    <img onClick={holdDice} style={styled} src={`/images/dice-${value}.png`} className="die" />
   )
 }
 
+const generateNewDice = () => {
+  const newDice = [];
+  for (let i = 0; i < 10; i++) {
+    const obj = { 
+      value: Math.floor((Math.random() * 6) + 1), 
+      isHeld: false,
+      id: nanoid()
+    };
+    
+    newDice.push(obj)
+  }
+  return newDice
+}
+
 const App = () => {
-  const generateRandom = () => Math.floor((Math.random() * 6) + 1);
+  const [dice, setDice] = useState(generateNewDice())
+
+  const holdDice = (id) => {
+    console.log(id)
+  }
+
+  const diceElements = dice.map(die => 
+    <Die key={die.id} value={die.value} isHeld={die.isHeld} holdDice={() => holdDice(die.id)} />
+  )
+
+  const rollDice = () => {
+    setDice(generateNewDice())
+  }
+
 
   return (
     <main>
     <h2>Tenzies</h2>
     <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
     <div className="container">
-    <Die random={generateRandom()} />
-    <Die random={generateRandom()} />
-    <Die random={generateRandom()} />
-    <Die random={generateRandom()} />
-    <Die random={generateRandom()} />
-    <Die random={generateRandom()} />
-    <Die random={generateRandom()} />
-    <Die random={generateRandom()} />
-    <Die random={generateRandom()} />
-    <Die random={generateRandom()} />
+      {diceElements}
     </div>
-    <button>Roll</button>
+    <button onClick={rollDice}>Roll</button>
     </main>
   )
 }
